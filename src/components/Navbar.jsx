458f8ede857062024e5b7e56e3ffce6a6e0099d2
sholadoye <shawlahrdoyet@gmail.com>
2021-01-31 15:17:26 +0100

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav>
-            <img src="./images/slack.svg" alt="Slack"/>
-            <a href="#slack">slack design</a>
+            <div className="slack">
+                <img src="./images/slack.svg" alt="Slack"/>
+                <a href="#slack">slack design</a>
+            </div>
             <span className="spacer"></span>
             <a href="#slack">Article</a>
             <a href="#slack">Our team</a>
