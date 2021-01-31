d0e2fab676ac449a87ebde18aef719ebb46e4e8e
sholadoye <shawlahrdoyet@gmail.com>
2021-01-31 14:25:59 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src="./images/slack.svg" alt="Slack"/>
+            <a href="#slack">slack design</a>
+            <span className="spacer"></span>
+            <a href="#slack">Article</a>
+            <a href="#slack">Our team</a>
+            <img src="./images/smile.svg" alt="smile"/>
+        </nav>
+    )
+}
+
+export default Navbar
